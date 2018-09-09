3e10bb97cd92d01ea862c7335f5822fd2e37fa00
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-09 16:59:49 -0400

    basics

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,20 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, 
+              private router: Router,
+              private route: ActivatedRoute ) { }
+  // Activated route injects currently active route
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    //this.router.navigate(['servers']); 
+    // Navigate method doesn't know where you are. It doesn't know what the currently loaded route is
+    // this.router.navigate(['servers', {relativeTo: this.route}]);
+    // Angular knows what our currently active route is because of the ActivatedRoute injection. Now, relative to this.route, navigate to server 
   }
 
 }
