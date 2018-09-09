c273f0da8fbb42b0049e7da86d70f6adfc8e3418
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-09 17:14:27 -0400

    destroy subscription

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,6 +9,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
+  paramsSubscription : Subscription;
 
   // ActivatedRoute: Currently loaded route 
   constructor(private route: ActivatedRoute) { }
@@ -18,13 +20,16 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     }
     // params is an observable - feature which allows you to work with async tasks
-    this.route.params.subscribe(
+    this.paramsSubscription = this.route.params.subscribe(
       (params: Params ) => { 
         this.user.id = params['id'];
         this.user.name = params['name'];
       }
-    )
+    ) // angular hangles destroying of this subscription. but for your own observables you have to do it
   }
 
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
 
 }
