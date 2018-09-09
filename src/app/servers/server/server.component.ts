1e95ac03b431131a5750ea317704e86456f72008
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-09 17:39:50 -0400

    convert to num, its string by default, the params

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -13,10 +13,10 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot['id'];
+    const id = +this.route.snapshot['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe( (params: Params ) => {
-        this.server = this.serversService.getServer(params['id']);
+        this.server = this.serversService.getServer(+params['id']);
     });
   }
 
