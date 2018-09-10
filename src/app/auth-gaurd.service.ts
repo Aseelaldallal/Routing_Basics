448472bae40c6284d8c557ce4018f059faf0be5f
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-09 21:11:49 -0400

    protect child route only:

--- src/app/auth-gaurd.service.ts
+++ src/app/auth-gaurd.service.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivateChild } from "@angular/router";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
@@ -6,7 +6,7 @@ import { AuthService } from "./auth.service";
 
 @Injectable({providedIn: 'root'})
 
-export class AuthGaurd implements CanActivate {
+export class AuthGaurd implements CanActivate, CanActivateChild {
 
     constructor(private authService: AuthService, private router : Router) {}
 
@@ -20,6 +20,11 @@ export class AuthGaurd implements CanActivate {
                      this.router.navigate(['/']);
                 }
             })
+    }
+
+    canActivateChild(route: ActivatedRouteSnapshot, 
+        state: RouterStateSnapshot) : Observable<boolean> | Promise<boolean> | boolean {
+            return this.canActivate(route, state);
     }
 
 }
